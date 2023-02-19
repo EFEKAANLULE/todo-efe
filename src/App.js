599dc705ea47f7f0493a3d747d6d1761db3d24f8
EFEKAANLULE <101347167+EFEKAANLULE@users.noreply.github.com>
2023-02-20 00:18:28 +0300

    fix: :art: added some new tasks and fixed some css
    Added new tasks, used props in daysleft.js and created tasks in app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import DaysLeft from './components/DaysLeft';
 
 function App() {
+
+  const tasks =[
+    { id:'t1', title:'Renew Insurance', daysleft:4,date: new Date(2021,2,21)},
+    { id:'t2',title:'Clean the House', daysleft:20,date: new Date(2020,4,29)},
+    { id:'t3',title:'Grocery Shopping', daysleft:29,date: new Date(2022,7,30)},
+    { id:'t4',title:'Donate to Charity', daysleft:52,date: new Date(2022,5,5)},
+
+  ];
   return (
     <div>
       
     <h1>LETS GET STARTED!</h1>
-    <DaysLeft></DaysLeft>
-    <DaysLeft></DaysLeft>
+    <DaysLeft title = {tasks[0].title} daysleft={tasks[0].daysleft} date={tasks[0].date} ></DaysLeft>
+    <DaysLeft title = {tasks[1].title} daysleft={tasks[1].daysleft} date={tasks[1].date} ></DaysLeft>
+    <DaysLeft title = {tasks[2].title} daysleft={tasks[2].daysleft} date={tasks[2].date} ></DaysLeft>
+    <DaysLeft title = {tasks[3].title} daysleft={tasks[3].daysleft} date={tasks[3].date} ></DaysLeft>
+
     </div>
   );
 }
--- src/components/DaysLeft.js
+++ src/components/DaysLeft.js
@@ -1,19 +1,15 @@
 import './DaysLeft.css';
 
-function DaysLeft() {
-
-    const daysDate = new Date(2021,2,28);
-    const taskTitle = 'Renew Insurance';
-    const daysLeft = 4;
+function DaysLeft(props) {
 
     return (
         <div className='days-left'>
             <div>
-            {daysDate.toISOString()} 
+            {props.date.toISOString()} 
             </div>
             <div className='days-left__description'>
-                <h2>{taskTitle} </h2>
-                <div className='days-left__price'>{daysLeft}</div>
+                <h2>{props.title} </h2>
+                <div className='days-left__days'>{props.daysleft}</div>
             </div>
         </div>
     );
