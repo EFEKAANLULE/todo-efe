1fbeec8b83b2616ab798d75d73fdb836d48e7e19
EFEKAANLULE <101347167+EFEKAANLULE@users.noreply.github.com>
2023-02-19 20:57:14 +0300

    firstdeneme

---
+++ src/components/DaysLeft.js
@@ -0,0 +1,19 @@
+import './DaysLeft.css';
+
+function DaysLeft() {
+
+    return (
+        <div className='days-left'>
+            <div>
+            Date 
+            </div>
+            <div className='days-left__description'>
+                <h2>Title </h2>
+                <div className='days-left__price'>Amount</div>
+            </div>
+        </div>
+    );
+}
+
+
+export default DaysLeft;
