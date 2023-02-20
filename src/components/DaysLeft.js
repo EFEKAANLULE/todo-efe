ecc2e84c566425dd2ef09e06d7ffd279b9eef135
EFEKAANLULE <101347167+EFEKAANLULE@users.noreply.github.com>
2023-02-20 13:22:45 +0300

    refactor: :white_check_mark: Understanding how onclick and console.log works with React
    Put out a ClickHandler function to change title h2, but only can see in console

--- src/components/DaysLeft.js
+++ src/components/DaysLeft.js
@@ -1,16 +1,26 @@
 import './DaysLeft.css';
 
+import './TasksDate';
+import TasksDate from './TasksDate';
 function DaysLeft(props) {
 
+    let title=props.title;
+
+    const clickHandler=() => {
+        console.log('Tıklandı !! ');
+        title='Updated!';
+        console.log(title);
+
+    }
     return (
         <div className='days-left'>
-            <div>
-            {props.date.toISOString()} 
-            </div>
+            <TasksDate date={props.date} ></TasksDate>
             <div className='days-left__description'>
                 <h2>{props.title} </h2>
+                <p> Days Left:</p>
                 <div className='days-left__days'>{props.daysleft}</div>
             </div>
+            <button onClick={clickHandler} >Change Title</button>
         </div>
     );
 }
---
+++ src/components/TasksDate.js
@@ -0,0 +1,22 @@
+
+function TasksDate(props) {
+
+    const month = props.date.toLocaleString('en-US',{month:'long'})
+    const day = props.date.toLocaleString('en-US',{day:'2-digit'})
+    const year = props.date.getFullYear()
+
+    return(
+
+
+        <div className='date'>
+                
+            <div>{day}</div>
+            <div>{month}</div>
+            <div>{year}</div>
+
+        </div>
+    );
+    
+}
+
+export default TasksDate;
